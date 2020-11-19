53c79c2f6f166a1141fba98a903b33f588a505c0
Guilherme <guilhermesantos0017@gmail.com>
2020-11-19 19:07:27 -0300

    Desafio 3

---
+++ build/desafio3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/guilherme/turtlebot-gazebo/src/desafio3
+# Build directory: /home/guilherme/turtlebot-gazebo/build/desafio3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/manifest.l"
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Action.l"
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionFeedback.l"
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionGoal.l"
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionResult.l"
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Feedback.l"
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Goal.l"
+  "/home/guilherme/turtlebot-gazebo/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Result.l"
+  "CMakeFiles/turtlebot3_example_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
